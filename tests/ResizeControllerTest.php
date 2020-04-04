af76720b36603fd86d8a6b246850ded8e5ea9b8a
theoboldt <erik@theoboldt.eu>
2020-04-04 11:22:52 +0200

    Added more possible results

--- tests/ResizeControllerTest.php
+++ tests/ResizeControllerTest.php
@@ -57,9 +57,12 @@ class ResizeControllerTest extends WebTestCase
         $response = $client->getResponse();
         $this->assertEquals(200, $response->getStatusCode());
 
-        $expected
-               = 'iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAACXBIWXMAAA7EAAAOxAGVKw4bAAABNklEQVQY0wXBy24TMRSA4d8+tsfNDE3DNRILxKqId6jYsEPiCVjwlvACLFkgISFxU8KiVJBpQzNJ5mL78H3m1cUz7XolegslcdjvmVXCogmMKbHeCu2/DlewOCmQB54+CGyD8HeXaHeKWGU5VzRbnJbC0PfMz4R7TYLkmAU4TsrnnyNv37xgGlbYGITHdyZevzxn/XtgtRlo94Xn53d5eL/h+vILHz5usKpgMpzGA+1Nwpieq3bL1bXB6MCn7z1/NkecF6GI8u79V05OE0+WS0JV8W11SS8Vy5mhcQEXg4esdKNy2w38WK1ZNB7VmkUMeBFiXeO6vsCQiF54NK8AyKqcxQnxnpSV21Gww+EGA4hRwCLW4sQx5URlR07ciMlHnGfHlDKTeFIBZxWMAI5f7Z461kxF+A/wVZKDbns1OwAAAABJRU5ErkJggg==';
-        $given = base64_encode($response->getContent());
-        $this->assertEquals($expected, $given);
+
+        $expected = [
+            'iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAACXBIWXMAAA7EAAAOxAGVKw4bAAABNklEQVQY0wXBy24TMRSA4d8+tsfNDE3DNRILxKqId6jYsEPiCVjwlvACLFkgISFxU8KiVJBpQzNJ5mL78H3m1cUz7XolegslcdjvmVXCogmMKbHeCu2/DlewOCmQB54+CGyD8HeXaHeKWGU5VzRbnJbC0PfMz4R7TYLkmAU4TsrnnyNv37xgGlbYGITHdyZevzxn/XtgtRlo94Xn53d5eL/h+vILHz5usKpgMpzGA+1Nwpieq3bL1bXB6MCn7z1/NkecF6GI8u79V05OE0+WS0JV8W11SS8Vy5mhcQEXg4esdKNy2w38WK1ZNB7VmkUMeBFiXeO6vsCQiF54NK8AyKqcxQnxnpSV21Gww+EGA4hRwCLW4sQx5URlR07ciMlHnGfHlDKTeFIBZxWMAI5f7Z461kxF+A/wVZKDbns1OwAAAABJRU5ErkJggg==',
+            'iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAACXBIWXMAAA7EAAAOxAGVKw4bAAABNklEQVQY0wXBy24TMRSA4d8+tsfNDE3DNRILxKqId6jYsEPiCVjwlvAC7NggISFxU8KiVJBpQzNJ5mL78H3m1cUz7XolegslcdjvmVXCogmMKbHeCu2/DlewOCmQB54+CGyD8HeXaHeKWGU5VzRbnJbC0B+ZnznuNQmSYxbgOCmff468ffOCaVxhYxAe30m8fnnO+vfAajPQ7gvPz+/y8H7D9eUXPnzcYFXBZDiNB9qbhDE9V+2Wq2uD0YFP33v+bI44L0IR5d37r5ycJp4sl4Sq4tvqkl4qljND4wIuBg9Z6Ublthv4sVqzaDyqNYsY8CLEusZ1fYEhEb3waF4BkFU5ixPiPSkrt6Ngh8MNBhCjgEWsxYljyonKjpy4EZOPOM+OKWUm8aQCzioYARy/2j11rJmK8B/xkpKEkBzgBgAAAABJRU5ErkJggg=='
+        ];
+        $given    = base64_encode($response->getContent());
+        $this->assertContains($given, $expected);
     }
 }
